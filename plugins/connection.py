cfc2a6e60efedfa6f577559825076b1159a61ad5
super7usr <glenna.lakin@blenched.com>
2025-04-09 23:55:04 +0100

    Update connection.py

--- plugins/connection.py
+++ plugins/connection.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
-from config import ADMINS
+from info import ADMINS
 
 @Client.on_message(filters.command("connect") & filters.private)
 async def connect_command(client, message):
